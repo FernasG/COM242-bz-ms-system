77ac425ccde5168c07cfcfdd57c485dd9169aafd
FernasG <fernandogoul7@gmail.com>
2023-11-19 22:16:56 -0300

    feat: add cron to rebalance street price

--- src/tasks-schedules/schedules.service.ts
+++ src/tasks-schedules/schedules.service.ts
@@ -30,4 +30,45 @@ export class ScheduleService {
     this.logger.log(`Usuário de nome: ${name} e registro: ${register}, foi o que obteve o melhor desempenho no mês anterior`);
     this.logger.log(`Pontos de gameificação foram zerados`);
   }
+
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  protected async rebalanceStreetParkingPrice() {
+    this.logger.log({ message: 'Rebalancing Street Parking Price', date: new Date() });
+    const [startDate, endDate] = [new Date(), new Date()];
+
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(23, 59, 59, 59);
+
+    const parkingSessions = await this.prismaService.parking_Session
+      .findMany({ where: { created_at: { gte: startDate, lte: endDate } } });
+
+    if (!parkingSessions.length) return null;
+
+    const groupStreetSessions = parkingSessions.reduce<{ [x: string]: { count: number; } }>((acc, current) => {
+      if (Object.keys(acc).includes(current.street_id)) acc[current.street_id].count += 1;
+      else acc[current.street_id] = { count: 1 };
+
+      return acc;
+    }, {});
+
+    const streetsPriceSum = Object.keys(groupStreetSessions).length * 3;
+    const parkingSessionsTotal = parkingSessions.length;
+
+    const streetsNewPricing = Object.entries(groupStreetSessions).reduce<{ street_id: string; price: number; }[]>((acc, current) => {
+      const [streetId, value] = current;
+      const percentage = value.count / parkingSessionsTotal;
+      const newPrice = streetsPriceSum * percentage;
+      acc.push({ street_id: streetId, price: newPrice });
+
+      return acc;
+    }, []);
+
+    this.prismaService.$transaction(
+      streetsNewPricing.map(({ street_id, price }) =>
+        this.prismaService.street.update({ where: { id: street_id }, data: { hour_price: price } })
+      )
+    );
+
+    this.logger.log({ message: 'Finalizing Rebalance Street Parking Price', date: new Date() });
+  }
 }
